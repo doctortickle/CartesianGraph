f6be5c3da739f53f2b4bffd2d40c9ca530d51e48
Dylan Russell <dyl.russell@gmail.com>
2018-02-01 11:12:00 -1000

    Alpha 0.1.2

--- src/graph/Hexagon.java
+++ src/graph/Hexagon.java
@@ -4,7 +4,13 @@ public class Hexagon implements EdgeMap {
 
 	@Override
 	public int[][] getDirs(int x, int y) {
-		return new int[][] { {1,0}, {0,1}, {-1,1}, {-1,0}, {0,-1}, {1,-1} };		
+		if( (x & 1) == 0) {
+			return new int[][] { {1,0}, {0,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1} };
+		}
+		else {
+			return new int[][] { {1,1}, {0,1}, {-1,1}, {-1,0}, {0,-1}, {1, 0} };
+		}
+				
 	}
 
 }
