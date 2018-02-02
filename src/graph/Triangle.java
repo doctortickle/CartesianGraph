a91fe39ad4cd76893457b268561b42f2f19f8baf
Dylan Russell <dyl.russell@gmail.com>
2018-02-02 10:00:06 -1000

    Alpha 0.1.5

--- src/graph/Triangle.java
+++ src/graph/Triangle.java
@@ -5,10 +5,10 @@ public class Triangle implements EdgeMap {
 	@Override
 	public int[][] getDirs(int x, int y) {
 		if( (x & 1) == 0 && (y & 1) == 0 || (x & 1) != 0 && (y & 1) != 0 ) {
-			return new int[][] { {1,0}, {-1,0}, {0,-1} };
+			return new int[][] { {1,0}, {-1,0}, {0,1} };
 		}
 		else {
-			return new int[][] { {1,0}, {-1,0}, {0,1} };
+			return new int[][] { {1,0}, {-1,0}, {0,-1} };
 		}
 	}
 
